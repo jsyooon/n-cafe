091df42f0359967a9cab763e53d566001b207f0d
jsyooon <jsyoonysj@gmail.com>
2023-07-04 23:34:30 +0900

    feat: [B] PUT /user 추가

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -19,6 +19,18 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.put('/', isAuth, async (req, res, next) => {
+  try {
+    if (!req.user) return res.status(401).json(null);
+    await User.update(req.body, {
+      where: { id: req.user.id },
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 router.get('/signup', isUnAuth, (req, res, next) => {
   const signupUser = req.session.signupUser;
 
@@ -59,10 +71,10 @@ router.post('/signup', isUnAuth, async (req, res, next) => {
   }
 });
 
-router.get('/name', async (req, res, next) => {
+router.get('/name', isAuth, async (req, res, next) => {
   try {
-    const name = await User.findOne({ where: { name: req.query.value } });
-    if (!name) return res.status(200).send('ok');
+    const existingUser = await User.findOne({ where: { name: req.query.value } });
+    if (!existingUser || existingUser.id === req.user.id) return res.status(200).send('ok');
     return res.status(409).send('이미 존재하는 닉네임입니다.');
   } catch (error) {
     next(error);
