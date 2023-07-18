a4a40cb8a13081133bfccba543fa5f40dbf093b0
jsyooon <jsyoonysj@gmail.com>
2023-07-18 23:33:36 +0900

    feat: [F] useOutsideClick hook 추가

---
+++ frontend/hooks/userOutSideClick.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import type { MutableRefObject } from 'react';
+
+export default function userOutSideClick(ref: MutableRefObject<Element>) {
+  const [outsideClick, setOutSideClick] = useState(false);
+
+  useEffect(() => {
+    const clickHandler = (event: Event) => {
+      if (ref.current) {
+        setOutSideClick(!ref.current.contains(event.target as Element));
+      }
+    };
+
+    document.addEventListener('click', clickHandler);
+
+    () => {
+      document.removeEventListener('click', clickHandler);
+    };
+  }, [ref]);
+
+  return outsideClick;
+}
