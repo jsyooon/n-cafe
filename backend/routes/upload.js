e9c63a1f5d9ecc601f17240f3d633854e43e5fa3
jsyooon <jsyoonysj@gmail.com>
2023-08-01 23:03:53 +0900

    feat: [B] /upload/feed 라우터 추가

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const sharp = require('sharp');
 const { profileUpload, feedUpload } = require('../upload');
 const { isAuth } = require('./middlewares');
 const { BACK_DOMAIN } = require('../config/const');
@@ -14,9 +15,24 @@ router.post('/profile', profileUpload.single('profile'), (req, res, next) => {
   }
 });
 
-router.post('/feed', isAuth, feedUpload.array('images', 10), (req, res, next) => {
+router.post('/feed', isAuth, feedUpload.array('images', 10), async (req, res, next) => {
   try {
-    return res.status(201).send(req.files.map(({ path }) => getFilePath(path)));
+    const data = await Promise.allSettled(
+      req.files.map(async ({ path }) => {
+        const metadata = await sharp(path).metadata();
+        return {
+          url: getFilePath(path),
+          width: metadata.width,
+          height: metadata.height,
+        };
+      })
+    );
+    return res.status(201).send(
+      data.map(({ status, ...result }) => {
+        if (status === 'fulfilled') return result.value;
+        return { error: result.reason };
+      })
+    );
   } catch (error) {
     return res.status(500).send('잠시 후 다시 시도해 주세요.');
   }
