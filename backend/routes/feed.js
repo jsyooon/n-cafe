c5f93e4a537e729f8d898f3a521a69ab308ad226
jsyooon <jsyoonysj@gmail.com>
2023-08-01 23:04:54 +0900

    feat: [B] post /feed 라우터에 이미지 매핑

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const { isAuth } = require('./middlewares');
 const { Feed } = require('../models');
+const { FeedImage } = require('../models');
 const router = express.Router();
 
 router.post('/', isAuth, async (req, res, next) => {
   try {
-    const post = await Feed.create({
+    const feed = await Feed.create({
       content: req.body.content,
       userId: req.user.id,
     });
 
-    return res.status(201).json(post);
+    if (req.body.images) {
+      await Promise.allSettled(
+        req.body.images.map((src) =>
+          FeedImage.create({
+            src,
+            feedId: feed.id,
+          })
+        )
+      );
+    }
+
+    return res.status(201).json(feed);
   } catch (error) {
     console.error(error);
     next(error);
