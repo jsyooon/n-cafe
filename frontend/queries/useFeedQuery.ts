ba1ffd7860b8104d22a790eb291c8ca9d53e2e8b
jsyooon <jsyoonysj@gmail.com>
2023-08-08 00:01:04 +0900

    feat: [F] GET /feed/all api 적용

---
+++ frontend/queries/useFeedQuery.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchGet } from '@/helpers/fetch';
+import type { CookieItemType } from '@/types/fetch';
+import type { FeedPreviewList } from '@/types/feed';
+
+export const FEEDS_LIST_QUERY_KEY = ['feedList'] as const;
+
+const fetchFeedList = (cookie?: CookieItemType) => fetchGet<FeedPreviewList>('/feed/all', cookie ? { cookie } : {}).then((response) => response?.data);
+
+export const fetchFeedListOnClient = () => fetchFeedList();
+
+export const fetchFeedListOnServer = (cookie: CookieItemType) => fetchFeedList(cookie);
+
+export const useFeedListQuery = () => useQuery({ queryKey: FEEDS_LIST_QUERY_KEY, queryFn: fetchFeedListOnClient });
