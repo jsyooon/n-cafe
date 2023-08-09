7b55237a1e74073273da845a21431068aa0d6a12
jsyooon <jsyoonysj@gmail.com>
2023-08-09 23:42:28 +0900

    feat: [F] ArticleHeader 컴포넌트 추가

---
+++ frontend/components/articleHeader/index.tsx
@@ -0,0 +1,28 @@
+import Writer from '@/components/writer';
+import { Dropdown, DropdownSelected, DropdownList } from '@/components/dropdown';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import type { WriterType } from '@/types/feed';
+import styles from './styles.module.scss';
+import type { PropsWithChildren } from 'react';
+
+interface Props {
+  writer: WriterType;
+  createdAt: string;
+}
+
+export default function articleHeader({ writer, createdAt, children }: PropsWithChildren<Props>) {
+  return (
+    <header className={styles.articleHeader}>
+      <Writer id={writer.id} name={writer.name} profileImage={writer.profileImage} />
+      <div className='date'>
+        <span>{new Intl.DateTimeFormat('ko-KR').format(new Date(createdAt))}</span>
+      </div>
+      <Dropdown className='util-wrap' inset='end'>
+        <DropdownSelected>
+          <FiMoreHorizontal size={18} />
+        </DropdownSelected>
+        <DropdownList>{children}</DropdownList>
+      </Dropdown>
+    </header>
+  );
+}
