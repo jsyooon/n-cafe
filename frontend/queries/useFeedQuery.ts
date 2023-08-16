3b1a4204925c08fa9db135114eb1e7927c9f1a48
jsyooon <jsyoonysj@gmail.com>
2023-08-16 23:50:30 +0900

    fix: [F] GET /feeds 라우터 적용

--- frontend/queries/useFeedQuery.ts
+++ frontend/queries/useFeedQuery.ts
@@ -5,7 +5,7 @@ import type { FeedPreviewList } from '@/types/feed';
 
 export const FEEDS_LIST_QUERY_KEY = ['feedList'] as const;
 
-const fetchFeedList = (cookie?: CookieItemType) => fetchGet<FeedPreviewList>('/feed/all', cookie ? { cookie } : {}).then((response) => response?.data);
+const fetchFeedList = (cookie?: CookieItemType) => fetchGet<FeedPreviewList>('/feeds', cookie ? { cookie } : {}).then((response) => response?.data);
 
 export const fetchFeedListOnClient = () => fetchFeedList();
 
