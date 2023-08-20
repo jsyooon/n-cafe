3cb4c99bb66f399116ce4deac478dcab6c6dc5ea
jsyooon <jsyoonysj@gmail.com>
2023-08-20 23:35:39 +0900

    fix: [F] 피드 미리보기 이미지 개수 제한 추가

--- frontend/components/feedImages/index.tsx
+++ frontend/components/feedImages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import type { FeedImageList } from '@/types/feed';
 import styles from './styles.module.scss';
+import { BsImage } from 'react-icons/bs';
 
 interface Props {
   images: FeedImageList;
@@ -9,7 +10,11 @@ interface Props {
 export default function FeedImages({ images }: Props) {
   const GRID_TYPE = ['columns', 'rows'];
 
+  const THUMBNAIL_LENGTH = 4;
+
   const [imageType, setImageType] = useState<number>();
+
+  const thumbnailLength = Math.min(images.length, THUMBNAIL_LENGTH);
 
   useEffect(() => {
     setImageType(+(images[0].width < images[0].height));
@@ -17,34 +22,40 @@ export default function FeedImages({ images }: Props) {
 
   return (
     <>
-      {images?.length > 0 && (
-        <div className={`image-wrap ${styles.imageWrap}`}>
+      {images.length > 0 && (
+        <div className={`image-wrap ${styles.feedImages}`}>
           <style jsx>
             {`
               .image-wrap {
-                ${images.length === 2 ? 'grid-template-columns: repeat(2, 1fr)' : ''}
+                ${thumbnailLength === 2 ? 'grid-template-columns: repeat(2, 1fr)' : ''}
                 ${
-                  images.length > 2
+                  thumbnailLength > 2
                     ? `
                 aspect-ratio: 10 / 9;
                 grid-template-${GRID_TYPE[GRID_TYPE.length - (imageType + 1)]}: repeat(2, 1fr);
-                grid-template-${GRID_TYPE.at(imageType * -1)}: repeat(${images.length - 1}, 1fr);
+                grid-template-${GRID_TYPE.at(imageType * -1)}: repeat(${thumbnailLength - 1}, 1fr);
                 `
                     : ''
                 }
   
                 .image-item:first-child {
-                  grid-${GRID_TYPE.at(imageType * -1).replace(/s$/, '')}: 1 / ${images.length};
+                  grid-${GRID_TYPE.at(imageType * -1).replace(/s$/, '')}: 1 / ${thumbnailLength};
                 }
               }
             `}
           </style>
 
-          {images.map(({ url }) => (
-            <div key={url.slice(url.lastIndexOf('/'))} className='image-item'>
+          {images.slice(0, THUMBNAIL_LENGTH).map(({ url }, i) => (
+            <div key={`${i}-${url.slice(url.lastIndexOf('/') + 1)}`} className='image-item'>
               <img src={url} alt='' />
             </div>
           ))}
+          {images.length > THUMBNAIL_LENGTH && (
+            <div className='more'>
+              <BsImage size={15} />
+              <span>+{images.length - THUMBNAIL_LENGTH}</span>
+            </div>
+          )}
         </div>
       )}
     </>
